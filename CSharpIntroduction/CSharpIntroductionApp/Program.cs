62cf15c002c222570e337bee81a3b6a8dd76bb14
smashing-statements <mjsdavies1@gmail.com>
2022-09-20 11:35:18 +0100

    Introductory Demo

--- CSharpIntroduction/CSharpIntroductionApp/Program.cs
+++ CSharpIntroduction/CSharpIntroductionApp/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            //PascalCase for methods and classes
+            //fields--variables--use camelCase
+            //private field _numPrivate
+
+            int x = 100;
+
+            x += 10;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("i: " + i + " x: " + x);
+                x += i;
+            }
+
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
         }
     }
 }
