5214b687b5257572ce657a59fd578802ef8f8ae8
smashing-statements <mjsdavies1@gmail.com>
2022-09-20 10:52:44 +0100

    created project

---
+++ CSharpIntroduction/CSharpIntroductionApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpIntroductionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
