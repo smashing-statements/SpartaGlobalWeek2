debf6b2aa98315e530b9ea02a7ae4088058cae6d
smashing-statements <mjsdavies1@gmail.com>
2022-09-20 14:50:34 +0100

    unit test lesson concluded

--- UnitTestLesson/UnitTestLessonTests/UnitTestLessonTests.cs
+++ UnitTestLesson/UnitTestLessonTests/UnitTestLessonTests.cs
@@ -4,6 +4,25 @@ namespace UnitTestLessonTests
 {
     public class Tests
     {
+
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(12)]
+        public void GivenATime_GetGreeting_ReturnsGoodMorning(int time)
+        {
+            //Arrange
+            //int time = 11;
+            string output;
+            string expectedOutput = "Good morning!";
+
+            //Act
+            output = Program.GetGreeting(time);
+
+            //Assert
+            Assert.That(output, Is.EqualTo(expectedOutput));
+        }
+
+        //Partition test for 19-24
         [Test]
         public void GivenATimeOf21_GetGreeting_ReturnsGoodEvening()
         {
@@ -18,5 +37,54 @@ namespace UnitTestLessonTests
             //Assert
             Assert.That(output, Is.EqualTo(expectedOutput));
         }
+
+        //Lower bound test for the 19-24 partition
+        [Test]
+        public void GivenATimeOf19_GetGreeting_ReturnsGoodEvening()
+        {
+            //Arrange
+            int time = 19;
+            string output;
+            string expectedOutput = "Good evening!";
+
+            //Act
+            output = Program.GetGreeting(time);
+
+            //Assert
+            Assert.That(output, Is.EqualTo(expectedOutput));
+        }
+
+        //upper bound of 0-4am partition
+        [Test]
+        public void GivenATimeOf4_GetGreeting_ReturnsGoodEvening()
+        {
+            //Arrange
+            int time = 4;
+            string output;
+            string expectedOutput = "Good evening!";
+
+            //Act
+            output = Program.GetGreeting(time);
+
+            //Assert
+            Assert.That(output, Is.EqualTo(expectedOutput));
+        }
+
+        //Partition test for 0-4 
+        [Test]
+        public void GivenATimeOf2_GetGreeting_ReturnsGoodEvening()
+        {
+            //Arrange
+            int time = 2;
+            string output;
+            string expectedOutput = "Good evening!";
+
+            //Act
+            output = Program.GetGreeting(time);
+
+            //Assert
+            Assert.That(output, Is.EqualTo(expectedOutput));
+        }
+
     }
 } 
