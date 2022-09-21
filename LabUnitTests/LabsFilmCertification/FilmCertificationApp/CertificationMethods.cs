d050e5e621820a1553a6970b0ea04dfb7867a215
smashing-statements <mjsdavies1@gmail.com>
2022-09-21 19:02:13 +0100

    finished BBFC labs

--- LabUnitTests/LabsFilmCertification/FilmCertificationApp/CertificationMethods.cs
+++ LabUnitTests/LabsFilmCertification/FilmCertificationApp/CertificationMethods.cs
@@ -7,19 +7,24 @@
             
         }
 
-        public static string GetClassification(int age)
+        public static string AvailableClassifications(int ageOfViewer)
         {
 
-            switch(age)
+            if (ageOfViewer < 0 || ageOfViewer > 150)
+            {
+                throw new ArgumentOutOfRangeException("Age: " + ageOfViewer + " Allowed age 0-150");
+            }
+
+            switch (ageOfViewer)
             {
                 case (< 12):
-                    return "U, PG & 12 films are available.";
+                    return "U, PG & 12A films are available.";
                 case (< 15):
-                    return "U, PG, 12 & 15 films are available.";
+                    return "U, PG, 12A & 12 films are available.";
                 case (< 18):
-                    return "U, PG, 12, 15 & 18 films are available.";
-                default:
-                    return "Invald age given";
+                    return "U, PG, 12A, 12 & 15 films are available.";
+                case (>= 18):
+                    return "All films are available.";
             }
         }
 
--- LabUnitTests/LabsFilmCertification/FilmCertificationTests/Classification_Tests.cs
+++ LabUnitTests/LabsFilmCertification/FilmCertificationTests/Classification_Tests.cs
@@ -12,17 +12,26 @@ namespace FilmCertificationTests
         [TestCase(-1)]
         public void GivenInvalidAge_ThrowOutOfBoundsException(int age)
         {
-            Assert.That(() => CertificationMethods.GetClassification(age), Throws.TypeOf<ArgumentOutOfRangeException>()
-                .With.Message.Contain("Allowed range 0-150"));
+            Assert.That(() => CertificationMethods.AvailableClassifications(age), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With.Message.Contain("Age: " + age + " Allowed age 0-150"));
         }
 
-        [TestCase(1, "U, PG, 12A")]
-        [TestCase(12, "U, PG, 12")]
-        [TestCase(15, "U, PG, 12, 15")]
-        [TestCase(18, "U, PG, 12, 15, 18")]
+        [TestCase(0, "U, PG & 12A films are available.")]
+        [TestCase(5, "U, PG & 12A films are available.")]
+        [TestCase(11, "U, PG & 12A films are available.")]
+
+        [TestCase(12, "U, PG, 12A & 12 films are available.")]
+        [TestCase(13, "U, PG, 12A & 12 films are available.")]
+        [TestCase(14, "U, PG, 12A & 12 films are available.")]
+
+        [TestCase(15, "U, PG, 12A, 12 & 15 films are available.")]
+        [TestCase(16, "U, PG, 12A, 12 & 15 films are available.")]
+        [TestCase(17, "U, PG, 12A, 12 & 15 films are available.")]
+
+        [TestCase(18, "All films are available.")]
         public void GivenNumberOfPounds_ReturnNumberOfStones(int age, string classification)
         {
-            Assert.That(CertificationMethods.GetClassification(age), Is.EqualTo(classification));
+            Assert.That(CertificationMethods.AvailableClassifications(age), Is.EqualTo(classification));
         }
     }
 }
