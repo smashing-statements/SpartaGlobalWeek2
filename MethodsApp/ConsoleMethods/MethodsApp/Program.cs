e6ad12724b7fe6a2048ef29ac3778c3965c32342
smashing-statements <mjsdavies1@gmail.com>
2022-09-23 16:01:16 +0100

    Phil's lesson

---
+++ MethodsApp/ConsoleMethods/MethodsApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.Runtime.Versioning;
+
+namespace MethodsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DoThis(10, "enthused");
+            var result2 = DoThis(25);
+            var pizza1 = OrderPizza(pineapple: true, anchovies: false);
+
+            //tuples
+            var myTuple = ("Joe", "Bloggs", 25);
+            Console.WriteLine(myTuple);
+            Console.WriteLine(myTuple.Item2);
+
+            var myNamedTuple = (fName: "Jane", lName: "Doe", age: 32);
+            Console.WriteLine(myNamedTuple);
+            Console.WriteLine(myNamedTuple.lName);
+
+            var title = "The Shining";
+            int copies = 3;
+            var myTuple3 = (title, copies);
+            Console.WriteLine(myTuple3);
+            Console.WriteLine(myTuple3.title);
+
+            var weight = ConvertPoundsToStones(146);
+
+        }
+
+        public static int DoThis(int x, string y = "happy")
+        {
+            //WriteLine is static
+            Console.WriteLine($"I'm feeling {y}");
+            return x * x;
+        }
+
+        public static string OrderPizza(bool anchovies, bool pineapple, bool mushrooms = false)
+        {
+            var pizza = "Pizza with tomato sauce, cheese, ";
+            if (anchovies) pizza += "anchovies, ";
+            if (anchovies) pizza += "pineapple, ";
+            if (anchovies) pizza += "mushrooms, ";
+            return pizza;
+        }
+
+        public static (int stones, int pounds) ConvertPoundsToStones(int pounds)
+        {
+            const int poundsInAStone = 14;
+            var st = pounds / poundsInAStone;
+            var lbs = pounds % poundsInAStone;
+            return (st, lbs);
+        }
+    }
+}
--- Strings_AndDataTypes_CodeALong/MoreDataTypes/MoreDataTypesApp/Program.cs
+++ Strings_AndDataTypes_CodeALong/MoreDataTypes/MoreDataTypesApp/Program.cs
@@ -1,10 +1,93 @@
-﻿namespace MoreDataTypesApp
+﻿using System;
+using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace MoreDataTypesApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myString = "  C# list fundamentals ";
+            //Console.WriteLine(StringExercise(myString));
+
+            int scoreA = 2;
+            int scoreB = 1;
+
+            //Console.WriteLine(StringBuilderExercise(myString));
+
+            string concatString1 = "Liverpool : " + scoreA + ", Manchester United : " + scoreB;
+            string concatString2 = String.Concat("Liverpool : ", scoreA, ", Manchester United : ", scoreB);
+
+            //StringInterpolation(myString);
+
+            ParsingStrings();
+        }
+
+        public static void StringInterpolation(string str)
+        {
+            //Console.WriteLine("My name is : " + str + "using +");
+            //Console.WriteLine($"My name is: {str} using interpolation");
+
+            var num1 = 2;
+            var num2 = 7;
+            var fString = $"{num1} to the power of {num2} is {Math.Pow(num1, num2)}";
+
+            //the below :#.0000 specifies print the integers to 3 decimal places
+            var fString2 = $"Log to the base {num2} of {num1} is {Math.Log(num2, num1):#.000}";
+            var fString3 = $"That will be {num2 / 3.0:C} please!";
+
+            Console.WriteLine(fString2);
+            Console.WriteLine(fString3);
+
+        }
+
+        public static void ParsingStrings()
+        {
+            //parsing strings
+            Console.WriteLine("How many apples?");
+            string input = Console.ReadLine();
+            //int numApples = Int32.TryParse(input, out int parsedApples)
+            var success = Int32.TryParse(input, out int parsedApples);
+        }
+    
+
+        public static string StringExercise(string myString)
+        {
+            //Always put myString = before using the method
+            myString = myString.Trim();
+            myString = myString.ToUpper();
+            myString = myString.Replace('L', '*');
+            myString = myString.Replace('T', '*');
+
+            //There is also a method called .LastIndexOf()
+            int index = (myString.IndexOf("N") + 1);
+            if (index > 0)
+                myString = myString.Substring(0, index);
+
+            return myString;
+
+        }
+
+        public static string StringBuilderExercise(string myString)
+        {
+            /*
+            int index = myString.IndexOf('n') + 1;
+            StringBuilder sb = new StringBuilder(myString);
+
+            sb.Replace('l', '*')
+              .Replace('t', '*')
+              .Remove(index, myString.Length - index);
+
+            return sb.ToString().Trim().ToUpper();
+            */
+
+            var trimmedUpperString = myString.Trim().ToUpper();
+            var nPos = trimmedUpperString.IndexOf('N');
+            StringBuilder sb = new StringBuilder(trimmedUpperString);
+            sb.Replace('L', '*').Replace('T', '*').Remove(nPos + 1, sb.Length - nPos - 1);
+            return sb.ToString();
+
         }
     }
 }
