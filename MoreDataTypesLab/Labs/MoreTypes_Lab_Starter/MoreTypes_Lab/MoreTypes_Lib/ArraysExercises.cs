047c9f458e524be5e18d061983964d0ddaec7417
smashing-statements <mjsdavies1@gmail.com>
2022-09-26 09:25:10 +0100

    finished array exercises

--- MoreDataTypesLab/Labs/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
+++ MoreDataTypesLab/Labs/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
@@ -21,13 +21,62 @@ namespace MoreTypes_Lib
         // returns a 3D array containing the contents of a given List
         public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
         {
-            throw new NotImplementedException();
+            string[,,] array1 = new string[length1, length2, length3];
+
+            if (length1 * length2 * length3 > 6)
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
+            else
+            {
+
+                for (int i = 0; i < length1; i++)
+                {
+                    for (int j = 0; j < length2; j++)
+                    {
+                        for (int k = 0; k < length3; k++)
+                        {
+                            //I don't know how this works but stack overflow helped me
+                            array1[i, j, k] = contents[i * length2 * length3 + j * length3 + k];
+                        }
+                    }
+                }
+
+                return array1;
+
+            }
         }
 
         // returns a jagged array containing the contents of a given List
         public static string[][] MakeJagged2DArray(int countRow1, int countRow2, List<string> contents)
         {
-            throw new NotImplementedException();
+            string[][] jaggedArray = new string[2][];
+            jaggedArray[0] = new string[countRow1];
+            jaggedArray[1] = new string[countRow2];
+
+            int listIterator = 0;
+
+            if (countRow1 + countRow2 > contents.Count)
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
+            else
+            {
+
+                //iterate through each row, in this case 2
+                for (int i = 0; i < jaggedArray.Length; i++)
+                {
+                    //iterate through the count of the current row
+                    for (int j = 0; j < jaggedArray[i].Length; j++)
+                    {
+                        //use an iterator independent of these loops to assign list items
+                        jaggedArray[i][j] = contents[listIterator++];
+                    }
+                }
+
+                return jaggedArray;
+
+            }
         }
     }
 }
